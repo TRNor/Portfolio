720a24a33d14404ca6e01cff59e26b11944df93b
TRNor <trnorman@our.ecu.edu.au>
2022-01-22 12:14:16 +0000

    Added practice script UVIndex Recommendation Calculator

---
+++ Scripts/Portfolio/Week 2/2.3 Scripts/Practice Scripts/UVIndex.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Ask user to input the UV index.
+read -p "Please enter a UV index value from 0 to 11: ' UVIndex
+
+if [ "$UVIndex" -le 2 ]; then
+	echo "Minimal protection required."
+elif [[ "$UVIndex" -ge 3 ] && [[ "$UVIndex" -le 7 ]]; then
+	echo "Standard protection required."
+elif [[ "$UVIndex" -ge 8 ]] && {{ "$UVIndex" -le 11 ]]; then
+	echo "Full protective measures required."
+else
+	echo "Invalid input"
+fi
+exit 0
