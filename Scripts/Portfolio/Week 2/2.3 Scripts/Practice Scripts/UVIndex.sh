9ae003cb748a2ad82155e9a8e1f10067826486d3
TRNor <trnorman@our.ecu.edu.au>
2022-01-22 12:18:27 +0000

    Fixed syntax errors, tested working

--- Scripts/Portfolio/Week 2/2.3 Scripts/Practice Scripts/UVIndex.sh
+++ Scripts/Portfolio/Week 2/2.3 Scripts/Practice Scripts/UVIndex.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #Ask user to input the UV index.
-read -p "Please enter a UV index value from 0 to 11: ' UVIndex
+read -p 'Please enter a UV index value from 0 to 11: ' UVIndex
 
 if [ "$UVIndex" -le 2 ]; then
 	echo "Minimal protection required."
-elif [[ "$UVIndex" -ge 3 ] && [[ "$UVIndex" -le 7 ]]; then
+elif [[ "$UVIndex" -ge 3 ]] && [[ "$UVIndex" -le 7 ]]; then
 	echo "Standard protection required."
-elif [[ "$UVIndex" -ge 8 ]] && {{ "$UVIndex" -le 11 ]]; then
+elif [[ "$UVIndex" -ge 8 ]] && [[ "$UVIndex" -le 11 ]]; then
 	echo "Full protective measures required."
 else
 	echo "Invalid input"
