d19ac4eb1722cf2b86957f131fff62e6e5320fa7
TRNor <trnorman@our.ecu.edu.au>
2022-01-22 05:51:56 +0000

    First attempt at hashing users password

--- Scripts/Portfolio/Week 2/2.2 Scripts/setPassword.sh
+++ Scripts/Portfolio/Week 2/2.2 Scripts/setPassword.sh
@@ -6,7 +6,7 @@ mkdir "$folderName"
 # Step 3: Ask the user to enter a password ensuring their input is hidden.
 read -sp 'Please enter your password: ' password
 # Step 4: Write the password input by the user to a file named secret.txt.
-echo "$password" > secret.txt
+echo "$password" | sha256sum > secret.txt
 #Step 5: Move secret.txt file into the user defined folder.
 mv secret.txt $folderName
 exit 0
