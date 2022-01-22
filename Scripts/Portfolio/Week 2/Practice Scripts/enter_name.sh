fdf21db0370908af34b02fed776f90b087bd2ebf
TRNor <trnorman@our.ecu.edu.au>
2022-01-22 03:01:41 +0000

    Week 2.1 Scripts (foldermaker.sh & user input practice scripts)

---
+++ Scripts/Portfolio/Week 2/Practice Scripts/enter_name.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Enter Name: "
+read Name
+echo "Entered Name: $Name"
+exit 0
---
+++ Scripts/Portfolio/Week 2/Practice Scripts/enter_username_and_password.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+read -p 'Enter Username: ' username
+read -sp 'Enter Password: ' password
+echo
+echo "Entered Username: $username"
+echo "Entered Password: $password"
+exit 0
