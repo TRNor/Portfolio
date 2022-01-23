23b4e25ef3a7f077d68f87a18fb21b2c7aed750b
TRNor <trnorman@our.ecu.edu.au>
2022-01-23 08:46:49 +0000

    Added 2.5 Scripts folder and begun work on menu.sh

---
+++ Scripts/Portfolio/Week 2/2.5 Scripts/menu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Assign variable to passwordCheck script
+passCheck="/home/student/ThomasNorman/Scripts/Portfolio/Week\ 2/2.4\ Scripts/passwordCheck.sh"
+#Run passwordCheck script by calling variable "passCheck"
+"$passCheck"
+
+exit 0
