bc14ddf534490cf18d4685ed900b18caa038a507
TRNor <trnorman@our.ecu.edu.au>
2022-01-23 04:07:25 +0000

    Updated passwordCheck script

--- Scripts/Portfolio/Week 2/2.4 Scripts/passwordCheck.sh
+++ Scripts/Portfolio/Week 2/2.4 Scripts/passwordCheck.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Ask the user to type a secret password (hide the input)
-
+	#TESTED WORKS
 read -sp 'Please enter your password: ' password
 
 #Hash entered password for later comparison
@@ -10,8 +10,11 @@ echo "$password" | sha256sum > passwordHash
 
 #Check the users password against the hash stored in 'secret.txt'
 
+	#Pull password hash from secret.txt to variable 'hashCheck'
 
-if [[ "$passwordHash" -eq cat secret.txt ]]; then
+cat secret.txt | hashCheck
+
+if [[ "$passwordHash" -eq "$hashCheck" ]]; then
 
 #If the user's password is correct, print "Access Granted" and quit with the exit code 0
 
@@ -19,7 +22,7 @@ if [[ "$passwordHash" -eq cat secret.txt ]]; then
 	exit 0
 
 #: If the user's password is incorrect, print "Access Denied" and quit with the exit code 1
-
+	#TESTED WORKS
 else
 
 	echo "Access Denied"
