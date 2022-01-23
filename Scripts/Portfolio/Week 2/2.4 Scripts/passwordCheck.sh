4e37eafe41c5f3acde269444a8ef6eead1e3db78
TRNor <trnorman@our.ecu.edu.au>
2022-01-23 06:21:57 +0000

    Cleaned up code and  comments in passwordCheck script

--- Scripts/Portfolio/Week 2/2.4 Scripts/passwordCheck.sh
+++ Scripts/Portfolio/Week 2/2.4 Scripts/passwordCheck.sh
@@ -1,30 +1,44 @@
 #!/bin/bash
 
+#----------------------------------------------------------
+#This program looks at an existing password hash stored in a text file (secret.txt) and compares it with a user's entered password.
+#If the user enters the correct password, the program prints "Access Granted" to screen and has an exit code 0.
+#If the user enters an incorrect password, the program prints "Access Denied" to screen and has an exit code 1.
+#The code contains commented out tests for each stage of code for development and debugging purposes.
+#The correct password is "myPassword"
+#----------------------------------------------------------
+
 #Assign stored password hash to variable 'passHashStored'
+
 passHashStored=$(cat secret.txt)
 
-echo "Hash of stored password is: $passHashStored"
+	#Test:
+	#echo "Hash of stored password is: $passHashStored"
+
+#------------------------
 
 #Ask the user to type a secret password (hide the input)
 #Prompt the user to enter a password and assign this entry to the variable 'passwordEntered'
 
 read -sp 'Please enter your password: ' passwordEntered
+echo
 
-echo 
+	#Test:
+	#echo 
+	#echo "You entered the password: $passwordEntered"
 
-echo "You entered the password: $passwordEntered"
+#------------------------
 
 #Hash and send that input to the variable 'passHashEntered'
 
-#echo "$passwordEntered" | sha256sum > passHashEntered
 passHashEntered=$(echo "$passwordEntered" | sha256sum)
 
-echo "Hash of your entered password is: $passHashEntered"
+	#Test:
+	#echo "Hash of your entered password is: $passHashEntered"
 
-#Check the user's password against the hash stored in 'secret.txt'
-	#Pull password hash from secret.txt to variable 'passwordStored'
+#------------------------
 
-
+#Check the hash of the password entered by the user against the hash stored in 'secret.txt'
 
 if [[ "$passHashEntered" == "$passHashStored" ]]; then
 
@@ -35,9 +49,12 @@ if [[ "$passHashEntered" == "$passHashStored" ]]; then
 
 #: If the user's password is incorrect, print "Access Denied" and quit with the exit code 1
 	#TESTED WORKS
+
 else
 
 	echo "Access Denied"
 	exit 1
 
 fi
+
+#----------------------------------------------------------
