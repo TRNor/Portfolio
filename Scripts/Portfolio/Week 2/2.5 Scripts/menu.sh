b0dc143bc26c3c1748fb786f8baad45d024918ef
TRNor <trnorman@our.ecu.edu.au>
2022-01-23 10:03:59 +0000

    Updated menu.sh, added functionality, user selection working, running other scripts. Modified setPassword to resetPassword.

--- Scripts/Portfolio/Week 2/2.5 Scripts/menu.sh
+++ Scripts/Portfolio/Week 2/2.5 Scripts/menu.sh
@@ -2,4 +2,30 @@
 
 #Run passwordCheck script
 ( "/home/student/ThomasNorman/Scripts/Portfolio/Week 2/2.4 Scripts/passwordCheck.sh" )
+
+if [[ $? -eq 0 ]]; then
+    clear
+    echo "1. Create a folder"
+    echo "2. Copy a folder"
+    echo "3. Set a password"
+else
+    echo "The password you entered was incorrect"
+fi
+
+#Read users input
+
+read userSelection
+
+#Run appropriate script based on users selection
+
+if [[ $userSelection -eq 1 ]]; then
+    ( "/home/student/ThomasNorman/Scripts/Portfolio/Week 2/2.1 Scripts/foldermaker.sh" )
+elif [[ $userSelection -eq 2 ]]; then
+    ( "/home/student/ThomasNorman/Scripts/Portfolio/Week 2/2.3 Scripts/foldercopier.sh" )
+elif [[ $userSelection -eq 3 ]];then
+    ( "/home/student/ThomasNorman/Scripts/Portfolio/Week 2/2.5 Scripts/resetPassword.sh" )
+else
+    echo "You entered an invalid option."
+fi
+
 exit 0
---
+++ Scripts/Portfolio/Week 2/2.5 Scripts/resetPassword.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Ask the user to enter a new password ensuring their input is hidden.
+read -sp 'Please enter your new password: ' password
+#Hash password input by the user and write it to a file named secret.txt.
+echo "$password" | sha256sum > secret.txt
+
+exit 0
