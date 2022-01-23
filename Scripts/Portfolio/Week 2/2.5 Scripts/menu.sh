eee4cc9e6f506eccc91cbc358cf699207a1a3962
TRNor <trnorman@our.ecu.edu.au>
2022-01-23 09:36:34 +0000

    Updated menu.sh and added secret.txt to successfully call passwordCheck script from menu script

--- Scripts/Portfolio/Week 2/2.5 Scripts/menu.sh
+++ Scripts/Portfolio/Week 2/2.5 Scripts/menu.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#Assign variable to passwordCheck script
-passCheck="/home/student/ThomasNorman/Scripts/Portfolio/Week\ 2/2.4\ Scripts/passwordCheck.sh"
-#Run passwordCheck script by calling variable "passCheck"
-"$passCheck"
-
+#Run passwordCheck script
+( "/home/student/ThomasNorman/Scripts/Portfolio/Week 2/2.4 Scripts/passwordCheck.sh" )
 exit 0
