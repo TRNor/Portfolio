6f3d79aafbeee180f1e1090bfe2448a3cda4600d
TRNor <trnorman@our.ecu.edu.au>
2022-01-23 10:10:28 +0000

    Updated resetPassword script to provide user feedback

--- Scripts/Portfolio/Week 2/2.5 Scripts/resetPassword.sh
+++ Scripts/Portfolio/Week 2/2.5 Scripts/resetPassword.sh
@@ -4,5 +4,8 @@
 read -sp 'Please enter your new password: ' password
 #Hash password input by the user and write it to a file named secret.txt.
 echo "$password" | sha256sum > secret.txt
+#User feedback
+echo
+echo "Your password was successfully changed."
 
 exit 0
