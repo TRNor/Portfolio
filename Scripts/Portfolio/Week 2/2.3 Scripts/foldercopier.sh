a2a882763842e1610612a27a416c02a5654ee8bd
TRNor <trnorman@our.ecu.edu.au>
2022-02-01 10:25:21 +0000

    Updated foldercopier and passwordCheck scripts with better comments & header and footer

--- Scripts/Portfolio/Week 2/2.3 Scripts/foldercopier.sh
+++ Scripts/Portfolio/Week 2/2.3 Scripts/foldercopier.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# foldercopier.sh - A basic script to copy a folder from one directory to another enabling the user to rename the new folder in the process.
+# foldercopier.sh - A basic script to make a copy of a folder enabling the user to rename the new folder in the process.
 # Author: Thomas Norman
 # Date Modified: 1/February/2022
 # ------------------------------------------------------------------------------------------
 
-# Purpose: Prompt user to enter the name of the folder they would like to copy.
+# Purpose: Prompt user to enter the name of the folder they would like to copy and assign this value to the variable "folderName".
 
 read -p "Type the name of the folder you would like to copy: " folderName
 
@@ -30,6 +30,6 @@ fi
 
 exit 0
 
-
+# ------------------------------------------------------------------------------------------
 # References
-# Dr. Dhir Shuchi (2022, January 24). Blog Post, Response to post by Thomas Norman "2.3 foldercopier trouble" Blackboard.Ecu.Edu.Au. https://blackboard.ecu.edu.au/webapps/blogs-journals/execute/viewBlog?course_id=_662967_1&blog_id=_151734_1&blog_course_user_id=_25217595_1&type=blogs&group_id=_199037_1&gml_reload=no&callBackUrl=&allMembers=false#entry_315687_1
+# MCKNIGHT, R. (2021, July 18). foldercopier.sh. Blackboard.Ecu.Edu.Au. https://blackboard.ecu.edu.au/webapps/blackboard/execute/displayLearningUnit?course_id=_662967_1&content_id=_8274248_1&mode=view
--- Scripts/Portfolio/Week 2/2.4 Scripts/passwordCheck.sh
+++ Scripts/Portfolio/Week 2/2.4 Scripts/passwordCheck.sh
@@ -1,60 +1,47 @@
 #!/bin/bash
+# foldercopier.sh - 
+    # This program looks at an existing password hash stored in a text file (secret.txt) and compares it with a user's entered password.
+    # If the user enters the correct password, the program prints "Access Granted" to screen and has an exit code 0.
+    # If the user enters an incorrect password, the program prints "Access Denied" to screen and has an exit code 1.
+    # The code contains commented out tests for each stage of code for development and debugging purposes.
+    # The correct password is "myPassword"
+# Author: Thomas Norman
+# Date Modified: 1/February/2022
+# ------------------------------------------------------------------------------------------
 
-#----------------------------------------------------------
-#This program looks at an existing password hash stored in a text file (secret.txt) and compares it with a user's entered password.
-#If the user enters the correct password, the program prints "Access Granted" to screen and has an exit code 0.
-#If the user enters an incorrect password, the program prints "Access Denied" to screen and has an exit code 1.
-#The code contains commented out tests for each stage of code for development and debugging purposes.
-#The correct password is "myPassword"
-#----------------------------------------------------------
-
-#Assign stored password hash to variable 'passHashStored'
+# Purpose: Assign stored password hash to variable 'passHashStored'
 
 passHashStored=$(cat secret.txt)
 
-	#Test:
-	#echo "Hash of stored password is: $passHashStored"
-
-#------------------------
-
-#Ask the user to type a secret password (hide the input)
-#Prompt the user to enter a password and assign this entry to the variable 'passwordEntered'
+# Purpose: Prompt the user to enter a password (hidden) and assign this entry to the variable 'passwordEntered'
 
 read -sp 'Please enter your password: ' passwordEntered
 echo
 
-	#Test:
-	#echo 
-	#echo "You entered the password: $passwordEntered"
-
-#------------------------
-
-#Hash and send that input to the variable 'passHashEntered'
+# Purpose: Hash and send that input to the variable 'passHashEntered'.
 
 passHashEntered=$(echo "$passwordEntered" | sha256sum)
 
-	#Test:
-	#echo "Hash of your entered password is: $passHashEntered"
-
-#------------------------
-
-#Check the hash of the password entered by the user against the hash stored in 'secret.txt'
+# Purpose: Check the hash of the password entered by the user against the hash stored in 'secret.txt'.
 
 if [[ "$passHashEntered" == "$passHashStored" ]]; then
 
-#If the user's password is correct, print "Access Granted" and quit with the exit code 0
+	# Purpose: If the user's password is correct, print "Access Granted" and quit with the exit code 0.
 
 	echo "Access Granted"
-	exit 0
 
-#: If the user's password is incorrect, print "Access Denied" and quit with the exit code 1
-	#TESTED WORKS
+	exit 0
 
 else
 
+	# Purpose: If the password is incorrect, print "Access Denied" and quit with the exit code 1.
+
 	echo "Access Denied"
-	exit 1
+
+		exit 1
 
 fi
 
-#----------------------------------------------------------
+# ------------------------------------------------------------------------------------------
+# References
+# MCKNIGHT, R. (2022, January 17). passwordCheck.sh. Blackboard.Ecu.Edu.Au. https://blackboard.ecu.edu.au/webapps/blackboard/execute/displayLearningUnit?course_id=_662967_1&content_id=_8274249_1
