eda9386001f3f327c6af41ec1bd98dc04f275d7e
TRNor <trnorman@our.ecu.edu.au>
2022-02-02 07:49:54 +0000

    Started debuggin and fixing errors.sh code

--- Scripts/Portfolio/Week 2/2.6 Scripts/errors.sh
+++ Scripts/Portfolio/Week 2/2.6 Scripts/errors.sh
@@ -1,25 +1,28 @@
 #!/bin/bash 
       
 secret='shhh' #Don't tell anyone! 
+
+read -s -p "what's the secret code?" 
+
+echo
       
 #if the user types in the correct secret, tell them they got it right! 
       
-if [ "$secret" = $REPLY ]; then 
+if [ $secret = "$REPLY" ]; then 
       
     echo "You got it right!" 
       
     correct=true 
       
-else     echo "You got it wrong :(" 
+else     
+
+    echo "You got it wrong :(" 
       
     correct=false 
       
 fi 
       
-read -s -p "what's the secret code?" 
-      
-echo 
-      
+  
 case $correct in 
       
 *) 
