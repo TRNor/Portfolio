8271cee5cdb2bae12f72768e8fb33c29d98ee17a
TRNor <trnorman@our.ecu.edu.au>
2022-02-02 11:37:25 +0000

    added shebang, header and references to colourTest.sh using snippets

---
+++ Scripts/Portfolio/Week 3/colourTest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# colourTest.sh - Sandbox script to test colour snippets from exercise 3.1.
+# Author: Thomas Norman
+# Date Modified: 2 Februrary 2022
+# ------------------------------------------------------------------------------------------
+# Purpose: To test colour snippets.
+
+
+
+
+
+# ------------------------------------------------------------------------------------------
+# References
+# MCKNIGHT, R. 17 January 2022. 3.1 Create a snippet in Visual Studio Code and use in the script writing process. Blackboard.Ecu.Edu.Au. https://blackboard.ecu.edu.au/webapps/blackboard/execute/displayLearningUnit?course_id=_662967_1&content_id=_8274254_1&mode=view
