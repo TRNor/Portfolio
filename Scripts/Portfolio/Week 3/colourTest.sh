03747d981e4c5f6491d989334e17a6cf9ac28d9e
TRNor <trnorman@our.ecu.edu.au>
2022-02-02 12:20:35 +0000

    Added some code sourced from online to play around with scripting with colours

--- Scripts/Portfolio/Week 3/colourTest.sh
+++ Scripts/Portfolio/Week 3/colourTest.sh
@@ -6,9 +6,37 @@
 # Purpose: To test colour snippets.
 
 
+#!/bin/bash
 
+# Ansi color code variables
+red="\e[0;91m"
+blue="\e[0;94m"
+expand_bg="\e[K"
+blue_bg="\e[0;104m${expand_bg}"
+red_bg="\e[0;101m${expand_bg}"
+green_bg="\e[0;102m${expand_bg}"
+green="\e[0;92m"
+white="\e[0;97m"
+bold="\e[1m"
+uline="\e[4m"
+reset="\e[0m"
 
+# horizontally expanded backgrounds
+echo -e "${blue_bg}${reset}"
+echo -e "${red_bg}${reset}"
+echo -e "${green_bg}${reset}"
+
+echo ""
+
+echo -e "${red}Hello World!${reset}"
+
+echo -e "${green}This is a sentence across"
+echo "three lines to show how an ansi color"
+echo -e "works across multiple lines with echo.${reset}"
+
+reset="\e[0m"
 
 # ------------------------------------------------------------------------------------------
 # References
 # MCKNIGHT, R. 17 January 2022. 3.1 Create a snippet in Visual Studio Code and use in the script writing process. Blackboard.Ecu.Edu.Au. https://blackboard.ecu.edu.au/webapps/blackboard/execute/displayLearningUnit?course_id=_662967_1&content_id=_8274254_1&mode=view
+# Antonio (GameTheory). 3 October 2019. BASH tips and snippets. techStop. https://techstop.github.io/bash-script-colors/
