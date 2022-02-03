d5f59aa8a33a090f70e4f8d8f873d4a8330ff771
TRNor <trnorman@our.ecu.edu.au>
2022-02-03 23:31:49 +0000

    Added megafoldermaker.sh and created week 4 5 and 6 folders

---
+++ Scripts/Portfolio/Week 3/megafoldermaker.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# megafoldermaker.sh - A looping script that an create multiple folders at once.
+# Author: Thomas Norman
+# Date Modified: 4 February 2022
+# ------------------------------------------------------------------------------------------
+      
+#If there aren't two arguments to the script 
+      
+if (( $#!=2 )); then 
+      
+    #Print an error and exit 
+      
+    echo "Error, provide two numbers" && exit 1 
+      
+fi 
+      
+#For every number between the first argument and the last 
+      
+for ((i = $1; i <= $2; i++)) 
+      
+do 
+      
+    #Create a new folder for that number 
+      
+    echo "Creating directory number $i" 
+      
+    mkdir "Week $i" 
+      
+done 
+     
+
+# ------------------------------------------------------------------------------------------
+# References
+# MCKNIGHT, R. 17 January 2022. 3.3 Using loops for repetition. Blackboard.Ecu.Edu.Au. https://blackboard.ecu.edu.au/webapps/blackboard/execute/displayLearningUnit?course_id=_662967_1&content_id=_8274256_1
