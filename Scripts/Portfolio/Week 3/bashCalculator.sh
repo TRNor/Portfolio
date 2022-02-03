62f32e8bfc8d818b5be1003c409ae72222f5e147
TRNor <trnorman@our.ecu.edu.au>
2022-02-03 12:39:43 +0000

    Major rewrite of bashCalculator.sh to simplify code, use a case statement and add improved user experience

--- Scripts/Portfolio/Week 3/bashCalculator.sh
+++ Scripts/Portfolio/Week 3/bashCalculator.sh
@@ -12,88 +12,108 @@ expand_bg="\e[K"
 blue_bg="\e[0;104m${expand_bg}"
 red_bg="\e[0;101m${expand_bg}"
 green_bg="\e[0;102m${expand_bg}"
+white_bg="\e[0;47m${expand_bg}"
 green="\e[0;92m"
 yellow="\e[0;33m"
+black="\e[0;30m"
 purple="\e[0;35m"
 white="\e[0;97m"
 bold="\e[1m"
 uline="\e[4m"
 reset="\e[0m"
 
-activeNum=0
+workingNum=0
+EXIT=0
+
 
 # Purpose: Print Menu to screen and prompt user to select arithmetic operand.
 
+clear
+
 echo -e "${uline}${bold} Bash Calculator ${reset}"
-echo -e "${blue}1. Addition (+)${reset}"
-echo -e "${green}2. Subtraction (-)${reset}"
-echo -e "${red}3. Multiplication (x)${reset}"
-echo -e "${purple}4. Division (/)${reset}"
+echo ""
+echo "Please enter your first number, followed by your operand of choice and then your second number. For example 10 - 3"
+#add this functionality
+#echo "If you have already performed a calculation, you can use the previous ans as either the first or second number by typing ans"
+echo "Alternatively type C and enter to clear the working space or Q and enter to quit the program"
+echo ""
+echo -e "${blue}(+) Addition${reset}"
+echo -e "${green}(-) Subtraction${reset}"
+echo -e "${red}(x) Multiplication${reset}"
+echo -e "${purple}(/) Division${reset}"
+echo -e "${yellow}(C) Clear${reset}"
+echo -e "${bold}(Q) QUIT ${reset}"
 echo ""
 
-# Purpose: Prompt user to select an arithmetic operand printed above and assign to variable "operandSelection".
 
-read -p 'Please select which arithmetic operand you wish to use: ' operandSelection
 
-# Purpose: Assign users choice to a variable (with various acceptable user inputs to indicate operand choice).
+# Purpose: While loop - run script until user decides to quit.
 
-if [ "$operandSelection" = "1" ] || [ "$operandSelection" = "add" ] || [ "$operandSelection" = "Add" ] || [ "$operandSelection" = "addition" ] || [ "$operandSelection" = "Addition" ] || [ "$operandSelection" = "+" ]; then
-    operandVar="add"
-elif [ "$operandSelection" = "2" ] || [ "$operandSelection" = "subtract" ] || [ "$operandSelection" = "Subtract" ] || [ "$operandSelection" = "subtraction" ] || [ "$operandSelection" = "Subtraction" ] || [ "$operandSelection" = "-" ]; then
-    operandVar="subtract"
-elif [ "$operandSelection" = "3" ] || [ "$operandSelection" = "multiply" ] || [ "$operandSelection" = "Multiply" ] || [ "$operandSelection" = "multiplication" ] || [ "$operandSelection" = "Multiplication" ] || [ "$operandSelection" = "x" ]; then
-    operandVar="multiply"
-elif [ "$operandSelection" = "4" ] || [ "$operandSelection" = "divide" ] || [ "$operandSelection" = "Divide" ] || [ "$operandSelection" = "division" ] || [ "$operandSelection" = "Division" ] || [ "$operandSelection" = "/" ]; then
-    operandVar="divide"
-else
-    echo -e "${bold}${red}Error! You have made an invalid entry${reset}"
+while [ "$EXIT" -eq 0 ]
+do
+
+# Purpose: Prompt user to type mathematical expression using two numbers and either the +, -, x or / operand.
+
+read -p '' firstNum operandVarSymbol secondNum
+echo "firstNum check: $firstNum"
+echo "operandVarSymbol check: $operandVarSymbol"
+echo "secondNum check: $secondNum"
+
+if [ $firstNum != "Q" ]; then
+    case $operandVarSymbol in
+    +)
+    workingNum=$(expr "$firstNum" + "$secondNum")
+    echo -e " = $workingNum"
+    ;;
+    -)
+    workingNum=$(expr "$firstNum" - "$secondNum")
+    echo -e " = $workingNum"
+    ;;
+    x)
+    workingNum=$(expr "$firstNum" x "$secondNum")
+    echo -e " = $workingNum"
+    ;;
+    /)
+    workingNum=$(expr "$firstNum" / "$secondNum")
+    echo -e " = $workingNum"
+    ;;
+    #Default Case
+    *)
+    echo ""
+    echo -e "${red}${bold}ERROR: Invalid entry!${reset}"
+    ;;
+    esac
+elif [ $firstNum = "C" ]; then
+    clear
+    echo -e "${uline}${bold} Bash Calculator ${reset}"
+    echo ""
+    echo "Please enter your first number, followed by your operand of choice and then your second number. For example 10 - 3"
+    echo "Alternatively type C and enter to clear the working space or Q and enter to quit the program"
+    echo ""
+    echo -e "${blue}(+) Addition${reset}"
+    echo -e "${green}(-) Subtraction${reset}"
+    echo -e "${red}(x) Multiplication${reset}"
+    echo -e "${purple}(/) Division${reset}"
+    echo -e "${yellow}(C) Clear${reset}"
+    echo -e "${bold}(Q) QUIT ${reset}"
+    echo ""
+    firstNum=0
+    secondNum=0
+    operandVarSymbol=?
+elif [ $firstNum = "Q" ]; then
+    echo "Program quit successfully"
+    EXIT=1
+else   
+    echo ""
+    echo -e "${red}${bold}ERROR: UNKNOWN ERROR!${reset}"
 fi
 
-# Purpose:  If statement to run appropriate mathematical calculation based on user's input.
 
-if [ "$operandVar" = "add" ]; then
-    # Purpose: Prompt user for input of two numbers, then calculate the addition of the two numbers and assign the result to a variable "activeNum" and print the result.
-    read -p "Please enter the two numbers you wish to $operandVar: " firstNum secondNum
-    activeNum=$(expr "$firstNum" + "$secondNum")
-    echo ""
-    echo -e "${yellow}Result: $firstNum ${blue}+${yellow} $secondNum = $activeNum"
-elif [ "$operandVar" = "subtract" ]; then
-    # Purpose: Prompt user for input of two numbers, then calculate the addition of the two numbers and assign the result to a variable "activeNum" and print the result.
-    read -p "Please enter the number you wish to $operandVar from followed by the number you wish to $operandVar by: " firstNum secondNum
-    activeNum=$(expr "$firstNum" - "$secondNum")
-    echo ""
-    echo -e "${yellow}Result: $firstNum ${green}-${yellow} $secondNum = $activeNum"
-elif [ "$operandVar" = "multiply" ]; then
-    # Purpose: Prompt user for input of two numbers, then calculate the addition of the two numbers and assign the result to a variable "activeNum" and print the result.
-    read -p "Please enter the number you wish to $operandVar with followed by the number you wish to $operandVar by: " firstNum secondNum
-    activeNum=$(expr "$firstNum" \* "$secondNum")
-    echo ""
-    echo -e "${yellow}Result: $firstNum ${red}x${yellow} $secondNum = $activeNum"
-elif [ "$operandVar" = "divide" ]; then
-    # Purpose: Prompt user for input of two numbers, then calculate the addition of the two numbers and assign the result to a variable "activeNum" and print the result.
-    read -p "Please enter the number you wish to $operandVar followed by the number you wish to $operandVar by: " firstNum secondNum
-    activeNum=$(expr "$firstNum" / "$secondNum")
-    echo ""
-    echo -e "${yellow}Result: $firstNum ${purple}/${yellow} $secondNum = $activeNum${reset}"
-else
-    echo -e "${bold}${red}Error! Something went wrong with the operandVar if statement${reset}"
-fi
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+done
 
 # ------------------------------------------------------------------------------------------
 # References
 # MCKNIGHT, R. 17 January 2022. 3.2 Use arithmetic expressions to do calculations in scripts. Blackboard.Ecu.Edu.Au. https://blackboard.ecu.edu.au/webapps/blackboard/execute/displayLearningUnit?course_id=_662967_1&content_id=_8274255_1
 # Antonio (GameTheory). 3 October 2019. BASH tips and snippets. techStop. https://techstop.github.io/bash-script-colors/
+# Linuxize. 24 February 2020. Bash While Loop. Linuxize. https://linuxize.com/post/bash-while-loop/
+
