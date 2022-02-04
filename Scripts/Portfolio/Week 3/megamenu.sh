e1aec76e958c450af65dfedb66a950135cbc0e8b
TRNor <trnorman@our.ecu.edu.au>
2022-02-04 10:22:18 +0000

    Scaffold for megamenu.sh written

---
+++ Scripts/Portfolio/Week 3/megamenu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# megamenu.sh - A larger script that presents the user with a user friendly interactive menu that can be quit at any time by the user and allows the user to open multiple other scripts.
+# Author: Thomas Norman
+# Date Modified: 4 February 2022
+# ------------------------------------------------------------------------------------------
+
+# Purpose: Define Global Variables
+
+# Purpose: Print menu to screen
+
+# Purpose: Prompt user for a selection
+
+# Purpose: Select appropriate script based on users selection (case statement)
+
