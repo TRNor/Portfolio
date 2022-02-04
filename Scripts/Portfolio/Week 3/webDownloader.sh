fc27d35b56fef8da60c2d95a1b8928b5596f086a
TRNor <trnorman@our.ecu.edu.au>
2022-02-04 08:08:54 +0000

    Finished webDownloader main function and tested successfully

--- Scripts/Portfolio/Week 3/webDownloader.sh
+++ Scripts/Portfolio/Week 3/webDownloader.sh
@@ -4,15 +4,25 @@
 # Date Modified: 4 February 2022
 # ------------------------------------------------------------------------------------------
 
-# Purpose: {Brief description of  the purpose of each block of script}
 
 
 
+# Purpose: Prompt the user to enter the URL of the file they wish to download.
 
+read -p 'Please enter the URL you wish to download: ' URL
 
+# Purpose: Prompt the user to enter the directory for where they wish to download the file to.
 
+read -p 'Please enter the destination directory you wish to download to: ' directory
+
+# Purpose: use wget command to download contents of URL input by the user to the directory input by the user (Ref 2)
+
+wget -P "$directory" "$URL"
 
 
 # ------------------------------------------------------------------------------------------
 # References
 # 1. MCKNIGHT, R. 17 January 2022. 3.5 An internet downloader using loops. Blackboard.Ecu.Edu.Au. https://blackboard.ecu.edu.au/webapps/blackboard/execute/displayLearningUnit?course_id=_662967_1&content_id=_8274258_1
+# 2. DOMANTAS G. 2 February 2022. Tutorials wget command examples. www.hostinger.com. https://www.hostinger.com/tutorials/wget-command-examples/
+
+
