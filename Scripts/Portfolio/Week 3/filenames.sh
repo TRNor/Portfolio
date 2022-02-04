ab2304e9513ba27d9dfb347deb64e1d8cff45eb6
TRNor <trnorman@our.ecu.edu.au>
2022-02-04 03:48:13 +0000

    successfuly counts lines in .txt file and assigns each line to an element of an array.

--- Scripts/Portfolio/Week 3/filenames.sh
+++ Scripts/Portfolio/Week 3/filenames.sh
@@ -13,18 +13,18 @@ reset="\e[0m"
 
 
 
-# Purpose: count number of lines in .txt file to itterate through.
-#lineCountRaw=$(wc -l filenames.txt)
-#lineCount=$($lineCountRaw | cut -d' ' -f1)
-#echo "$lineCount"
+# Purpose: count number of lines in .txt file to itterate through (Ref 5).
+lineCount=$(wc -l < filenames.txt)
 
-# Purpose: write each line of the .txt file to an array using the mapfile function.
+echo "$lineCount"
+
+# Purpose: Write each line of the filenames.txt file to an array using the mapfile function (Ref 4 and.
 
 mapfile -t lineArray < <(cut -d' ' -f1 < filenames.txt)
 
 # Purpose: For each line (array element) compare to various conditions and output result.
 
-for ((i = 0; i <= 10; i++))
+for ((i = 0; i <= $lineCount; i++))
 do
 echo ${lineArray[$i]}
 done
@@ -36,9 +36,9 @@ exit 0
 
 # ------------------------------------------------------------------------------------------
 # References
-# MCKNIGHT, R. 17 January 2022. 3.2 Use arithmetic expressions to do calculations in scripts. Blackboard.Ecu.Edu.Au. https://blackboard.ecu.edu.au/webapps/blackboard/execute/displayLearningUnit?course_id=_662967_1&content_id=_8274255_1
-# Antonio (GameTheory). 3 October 2019. BASH tips and snippets. techStop. https://techstop.github.io/bash-script-colors/
-# YASIN, A. November 2021. count the number of lines in a file in bash. linuxhint. https://linuxhint.com/count-the-number-of-lines-in-a-file-in-bash/
-# CYRUS. 12 October 2019. read lines from a text file and store it in array. stackoverflow. https://stackoverflow.com/questions/58352217/read-lines-from-text-file-and-store-it-in-array
-
+# 1. MCKNIGHT, R. 17 January 2022. 3.2 Use arithmetic expressions to do calculations in scripts. Blackboard.Ecu.Edu.Au. https://blackboard.ecu.edu.au/webapps/blackboard/execute/displayLearningUnit?course_id=_662967_1&content_id=_8274255_1
+# 2. Antonio (GameTheory). 3 October 2019. BASH tips and snippets. techStop. https://techstop.github.io/bash-script-colors/
+# 3. YASIN, A. November 2021. count the number of lines in a file in bash. linuxhint. https://linuxhint.com/count-the-number-of-lines-in-a-file-in-bash/
+# 4. CYRUS. 12 October 2019. read lines from a text file and store it in array. stackoverflow. https://stackoverflow.com/questions/58352217/read-lines-from-text-file-and-store-it-in-array
+# 5. MORTON, B. 2 November 2021. Bash Split String. linuxscrew. https://www.linuxscrew.com/bash-split-string
 
