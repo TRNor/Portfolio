51a930004e32a83c1fe87702d49f9f6a9c660913
TRNor <trnorman@our.ecu.edu.au>
2022-02-05 13:33:17 +0000

    Finished megamenu functionality. improve linked scripts for better user experience

--- Scripts/Portfolio/Week 3/megamenu.sh
+++ Scripts/Portfolio/Week 3/megamenu.sh
@@ -8,7 +8,7 @@
 
 red="\e[0;91m"
 blue="\e[0;94m"
-blue_bd="\e[1;34m"
+green_bd="\e[1;32m"
 green="\e[0;92m"
 yellow="\e[0;33m"
 cyan="\e[0;36m"
@@ -22,16 +22,13 @@ clear
 
 # Purpose: Check Password
 
-#if /home/student/ThomasNorman/Scripts/Portfolio/Week 2/passwordCheck.sh; then
-
 ( "/home/student/ThomasNorman/Scripts/Portfolio/Week 2/passwordCheck.sh" )
 
-
-# If password is correct then start program
+# If password is correct then start program after 0.5 seconds.
 
 if [ $? = 0 ]; then
 
-sleep 0.8s
+sleep 0.5s
 
 clear
 
@@ -42,17 +39,16 @@ do
 #clear
 echo -e "Mega Menu" | figlet
 echo ""
-echo -e "${blue_bd}Select an option: ${reset}"
+echo -e "${green_bd}Select an option: ${reset}"
 echo -e "${cyan} 1) Create a folder"
 echo -e " 2) Copy a folder"
-echo -e " 3) Set a password"
+echo -e " 3) Reset the password"
 echo -e " 4) Calculator"
 echo -e " 5) Create week folders"
 echo -e " 6) Check filenames"
 echo -e " 7) Download a file${reset}"
-echo -e "${yellow} 8) QUIT${reset}"
+echo -e "${yellow} Q) QUIT${reset}"
 echo ""
-
 
 # Purpose: Save user selection to variable
 read -p '' userSelection
@@ -63,40 +59,58 @@ case $userSelection in
 1)
     # Create a folder
     ( "/home/student/ThomasNorman/Scripts/Portfolio/Week 2/foldermaker.sh" )
+    sleep 1s
+    clear
 ;;
 2)
     # Copy a folder
     ( "/home/student/ThomasNorman/Scripts/Portfolio/Week 2/foldercopier.sh" )
+    sleep 1s
+    clear
 ;;
 3)
     # Reset the password
     ( "/home/student/ThomasNorman/Scripts/Portfolio/Week 2/resetPassword.sh" )
+    sleep 1s
+    clear
 ;;
 4)
     # Calculator
     ( "/home/student/ThomasNorman/Scripts/Portfolio/Week 3/bashCalculator.sh" )
+    sleep 1s
+    clear
 ;;
 5)
     # Create week folders
     ( "/home/student/ThomasNorman/Scripts/Portfolio/Week 3/megafoldermaker.sh" )
+    sleep 1s
+    clear
 ;;
 6)
     # Check filenames
     ( "/home/student/ThomasNorman/Scripts/Portfolio/Week 3/filenames.sh" )
+    sleep 3s
+    clear
 ;;
 7)
     # Download a file
     ( "/home/student/ThomasNorman/Scripts/Portfolio/Week 3/webDownloader.sh" )
+    sleep 1s
+    clear
 ;;
-8)
+Q)
     # Quit
     clear
-    echo "Program successfully quit."
+    echo "Program quit successfully."
     break
 ;;
 #Default Case
 *)
     # Print error message
+    clear
+    echo -e "${red}INVALID USER INPUT!${reset}"
+    sleep 1s
+    clear
 ;;
 esac
 
@@ -106,15 +120,9 @@ else
     :
 fi
 
-
 exit 0
-
-
-
-
-
-
 
 # ------------------------------------------------------------------------------------------
 # References
-# {Last Name}, {First Name Initial}. {date published}. {name of referenced material}. {name of location of referenced material}. {URL of referenced material}
+# MCKNIGHT, R. 17 January 2022. 3.6 Mega menu. Blackboard.Ecu.Edu.Au. https://blackboard.ecu.edu.au/webapps/blackboard/execute/displayLearningUnit?course_id=_662967_1&content_id=_8274259_1&mode=view
+# CHADWICK, R. 2022. Bash Scripting Tutorial - 6. Loops. ryanstutorials.net. https://ryanstutorials.net/bash-scripting-tutorial/bash-loops.php
