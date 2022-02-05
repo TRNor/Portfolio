445965574efdce532e0273093001e287307bf2a7
TRNor <trnorman@our.ecu.edu.au>
2022-02-05 11:08:35 +0000

    Fixed references

--- Scripts/Portfolio/Week 3/filenames.sh
+++ Scripts/Portfolio/Week 3/filenames.sh
@@ -4,11 +4,11 @@
 # Date Modified: 5 February 2022
 # ------------------------------------------------------------------------------------------
 
-# Purpose: Write each line of the filenames.txt file to an array using the mapfile function (Ref 4).
+# Purpose: Write each line of the filenames.txt file to an array using the mapfile function (Ref 2).
 
 mapfile -t lineArray < filenames.txt
 
-# Purpose: For each line (array element) compare to various conditions using if/else statement and file operators. (Ref 5)
+# Purpose: For each line (array element) compare to various conditions using if/else statement and file operators. (Ref 3)
 
 for i in ${lineArray[@]};
 do
@@ -30,7 +30,5 @@ exit 0
 # ------------------------------------------------------------------------------------------
 # References
 # 1. MCKNIGHT, R. 17 January 2022. 3.4 Write for loop statements to control program flow. Blackboard.Ecu.Edu.Au. hhttps://blackboard.ecu.edu.au/webapps/blackboard/execute/displayLearningUnit?course_id=_662967_1&content_id=_8274257_1
-# 2. ANTONIO. (GameTheory). 3 October 2019. BASH tips and snippets. techStop. https://techstop.github.io/bash-script-colors/
-# 3. YASIN, A. November 2021. count the number of lines in a file in bash. linuxhint. https://linuxhint.com/count-the-number-of-lines-in-a-file-in-bash/
-# 4. CYRUS. 12 October 2019. read lines from a text file and store it in array. stackoverflow. https://stackoverflow.com/questions/58352217/read-lines-from-text-file-and-store-it-in-array
-# 5. COMPUTERHOPE. 1 February 2021. Bash test builtin command. computerhope. https://www.computerhope.com/unix/bash/test.htm
+# 2. CYRUS. 12 October 2019. read lines from a text file and store it in array. stackoverflow. https://stackoverflow.com/questions/58352217/read-lines-from-text-file-and-store-it-in-array
+# 3. COMPUTERHOPE. 1 February 2021. Bash test builtin command. computerhope. https://www.computerhope.com/unix/bash/test.htm
