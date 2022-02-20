a60995768f57b45ef01594b84dfe81fbacfe86a9
TRNor <trnorman@our.ecu.edu.au>
2022-02-20 02:45:45 +0000

    Adjusted formatting for final portfolio submission

---
+++ Scripts/Portfolio/Week 1/Second.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# UserAccounts.sh - This script demonstrates the use of the echo command and special numeric variables.
+# Author: Thomas Norman
+# Date Modified: 20-02-2022
+# -----------------------------------------------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------------------------------------------------
+# Run this script (./Second.sh "name") and replace "name" with your name.
+#------------------------------------------------------------------------------------------------------------------------
+
+echo "Hi there!"
+echo "It's nice to see you $1!"
+exit 0
+
+# -----------------------------------------------------------------------------------------------------------------------
+# References
+# 1. 1.4 Write and execute simple shell scripts. (2022). https://blackboard.ecu.edu.au/webapps/blackboard/execute/displayLearningUnit?course_id=_662967_1&content_id=_8274243_1&mode=view
